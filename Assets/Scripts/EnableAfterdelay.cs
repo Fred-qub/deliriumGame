456f045f5d526a55f61589c28e293fc667caea20
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2026-03-13 21:04:25 +0000

    Added moves & delays to all interactions
    Added delays to all interactions, activating/deactivating objects, etc, so doesn't happen til doctor gets there.

--- Assets/Scripts/EnableAfterdelay.cs
+++ Assets/Scripts/EnableAfterdelay.cs
@@ -4,31 +4,65 @@ public class EnableAfterdelay : MonoBehaviour
 {
     public GameObject poisonOnTrolley;
     public GameObject poisonInHand;
-    public float delaySeconds = 1f;
+    public GameObject lights;
+    public GameObject hearingAidTrolley;
+    public GameObject hearingAidHand;
+    public GameObject coatHangerEmpty;
+    public GameObject coatOnChair;
+    public float delayPoison = 1f;
+    public float delayLights = 1f;
+    public float delayHearingAid = 1f;
+    public float delayCoat = 1f;
 
-    public void EnableSequence() 
+    public void Poison()
     {
-        Invoke(nameof(EnablePoison), delaySeconds);
-
+        StartCoroutine(PoisonSequence());
     }
 
-    public void DisableSequence()
+    public void Lights()
     {
-        Invoke(nameof(DisablePoison), delaySeconds);
-
+        StartCoroutine(LightSequence());
     }
 
-
-    private void EnablePoison()
+    public void HearingAid()
     {
+        StartCoroutine(HearingAidSequence());
+    }
+
+    public void Coat()
+    {
+        StartCoroutine(CoatSequence());
+    }
+
+    private System.Collections.IEnumerator PoisonSequence()
+    {
+        yield return new WaitForSeconds(delayPoison);
         poisonInHand.SetActive(true);
-        
-    }
-    private void DisablePoison()
-    {
         poisonOnTrolley.SetActive(false);
-
+        yield return new WaitForSeconds(delayPoison);
+        poisonInHand.SetActive(false);
     }
 
+    private System.Collections.IEnumerator LightSequence()
+    {
+        yield return new WaitForSeconds(delayLights);
+        lights.SetActive(true);
+    }
 
+    private System.Collections.IEnumerator HearingAidSequence()
+    {
+        yield return new WaitForSeconds(delayHearingAid);
+        hearingAidHand.SetActive(true);
+        hearingAidTrolley.SetActive(false);
+        yield return new WaitForSeconds(delayHearingAid);
+        hearingAidHand.SetActive(false);
+    }
+
+    private System.Collections.IEnumerator CoatSequence()
+    {
+        yield return new WaitForSeconds(delayCoat);
+        coatHangerEmpty.SetActive(true);
+        coatOnChair.SetActive(true);
+
+    }
 }
