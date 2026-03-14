ac0f6245f968d09bc703cde0e5ccc3971636e413
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2026-03-14 21:50:09 +0000

    Remove unneeded items
    Spawnmanager removed from patient scene, also some commented out lines cleaned up from hallucination chance script

--- Assets/Scripts/HallucinationChance.cs
+++ Assets/Scripts/HallucinationChance.cs
@@ -7,8 +7,6 @@ public class HallucinationChance : MonoBehaviour
 {
     private int auxHallucinationChancePercentage = 50; // chance of an auxilliary hallucination occuring on a bad choice
     private int hallucinationTypeChancePercentage = 50; // chance of particular hallucination occurring
-    //public SpawnRat spawnRat;
-    //public SpawnSnake spawnSnake;
     public InteractionMaster trustManager;
     private string rat = "RatHallucination";
     private string snake = "SnakeHallucination";
@@ -47,16 +45,12 @@ public class HallucinationChance : MonoBehaviour
         if (Random.Range(0f, 100f) < hallucinationTypeChancePercentage) // if random number between 0 & 100 is less than the hallucination chance percentage
         {                  
                 Debug.Log("SpawnRat");
-                trustManager.interactionHistory.Add(rat);
-                //spawnRat.StartSpawn();  // rat (or rat alternative) is spawned
-                
+                trustManager.interactionHistory.Add(rat);               
         }
 
         else
         {
             Debug.Log("Spawn Snake"); 
-            //spawnSnake.Newspaper();
-            //spawnSnake.Snake(); // otherwise a snake is spawned
             trustManager.interactionHistory.Add(snake);
         }
     }
