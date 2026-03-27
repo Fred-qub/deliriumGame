9b9dbd2b26a275303e4a3446d2855b7287cabdd9
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2026-03-27 22:17:59 +0000

    Add sounds to transitions
    Knock, door open/close

--- Assets/Scripts/Transistions/LevelLoader.cs
+++ Assets/Scripts/Transistions/LevelLoader.cs
@@ -5,7 +5,13 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
-    public float transitionTime = 1f;
+    public float transitionTime = 2f;
+
+    [Header("Assign your audio clips here")]
+    public AudioClip firstClip;
+    public AudioClip secondClip;
+
+    private AudioSource audioSource;
 
     // Update is called once per frame
     void Update()
@@ -23,10 +29,29 @@ public class LevelLoader : MonoBehaviour
 
     IEnumerator LoadLevel(int levelIndex)
     {
+        audioSource = GetComponent<AudioSource>();
         transition.SetTrigger("End");
-        
+
+        StartCoroutine(PlaySoundsSequentially());
         yield return new WaitForSeconds(transitionTime);
-        
         SceneManager.LoadScene(levelIndex);
+    }
+
+   IEnumerator PlaySoundsSequentially()
+    {
+        // Play first clip
+        audioSource.clip = firstClip;
+        audioSource.Play();
+
+        // Wait until the first clip finishes
+        yield return new WaitForSeconds(firstClip.length);
+
+        // Play second clip
+        audioSource.clip = secondClip;
+        audioSource.Play();
+
+        yield return new WaitForSeconds(secondClip.length);
+
+
     }
 }
