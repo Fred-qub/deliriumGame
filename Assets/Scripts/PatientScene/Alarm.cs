97e8830daaf93c43e959731920085c781fbedb0c
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2026-03-29 15:09:50 +0100

    Add yellow & green flashing lights to monitor

--- Assets/Scripts/PatientScene/Alarm.cs
+++ Assets/Scripts/PatientScene/Alarm.cs
@@ -4,24 +4,22 @@ using System.Collections;
 public class Alarm : MonoBehaviour
 {
 
-    public GameObject alarmLamp;
+    public GameObject redAlarmLamp;
+    public GameObject yellowAlarmLamp;
+    public GameObject greenAlarmLamp;
     public GameObject normalScreen52;
     public GameObject normalScreen71;
     public GameObject normalScreen82;
     public GameObject normalScreen107;
     public GameObject alarmScreen121;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-      //  heartbeat = heartBeatScript.Heartbeat; 
- 
-    }
+    public float flashDuration = 0.1f;
+    public float beatInterval;
+    private float timer;
 
     private void OnEnable()
     {
         HeartBeat.OnHeartRateChanged += ChangeScreen;
+
     }
 
     private void OnDisable()
@@ -30,38 +28,101 @@ public class Alarm : MonoBehaviour
     }
 
 
-    public IEnumerator Flash()
+    public IEnumerator FlashRed()
     {
-        alarmLamp.gameObject.SetActive(true);
+        redAlarmLamp.gameObject.SetActive(true);
         yield return new WaitForSeconds(0.5f);
-        alarmLamp.gameObject.SetActive(false);
+        redAlarmLamp.gameObject.SetActive(false);
         yield return new WaitForSeconds(0.5f);
-        StartCoroutine(Flash());
+        StartCoroutine(FlashRed());
     }
 
-    private void ChangeScreen(int heartbeat) 
+    public IEnumerator FlashYellow()
     {
+        yellowAlarmLamp.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        yellowAlarmLamp.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(FlashYellow());
+    }
+
+    public IEnumerator FlashGreen(int heartbeat)
+    {
+        switch (heartbeat)
+        {
+            case 0: //52bpm
+                beatInterval = 60 / 52f;
+                while (true)
+                {
+                    greenAlarmLamp.gameObject.SetActive(true);
+                    yield return new WaitForSeconds(flashDuration);
+                    greenAlarmLamp.gameObject.SetActive(false);
+                    yield return new WaitForSeconds(beatInterval - flashDuration);
+                }
+            case 1: //71bpm
+                beatInterval = 60 / 71f;
+                while (true)
+                {
+                    greenAlarmLamp.gameObject.SetActive(true);
+                    yield return new WaitForSeconds(flashDuration);
+                    greenAlarmLamp.gameObject.SetActive(false);
+                    yield return new WaitForSeconds(beatInterval - flashDuration);
+                }
+            case 2: //82 bpm
+                beatInterval = 60 / 82f;
+                while (true)
+                {
+                    greenAlarmLamp.gameObject.SetActive(true);
+                    yield return new WaitForSeconds(flashDuration);
+                    greenAlarmLamp.gameObject.SetActive(false);
+                    yield return new WaitForSeconds(beatInterval - flashDuration);
+                }
+
+        }
+    }
+        
+        private void ChangeScreen(int heartbeat) 
+        {
         switch (heartbeat)
         {
             case 0: // 52 bpm
                      normalScreen52.gameObject.SetActive(true); normalScreen71.gameObject.SetActive(false); normalScreen82.gameObject.SetActive(false); normalScreen107.gameObject.SetActive(false); alarmScreen121.gameObject.SetActive(false);
                 Debug.Log("screen 52 bpm");
+                StopAllCoroutines();
+                yellowAlarmLamp.gameObject.SetActive(false);
+                redAlarmLamp.gameObject.SetActive(false);             
+                StartCoroutine(FlashGreen(heartbeat));      
                 break;
             case 1: // 71 bpm
                      normalScreen52.gameObject.SetActive(false); normalScreen71.gameObject.SetActive(true); normalScreen82.gameObject.SetActive(false); normalScreen107.gameObject.SetActive(false); alarmScreen121.gameObject.SetActive(false);
-                Debug.Log("screen 71 bpm");
+                    Debug.Log("screen 71 bpm");
+                StopAllCoroutines();
+                yellowAlarmLamp.gameObject.SetActive(false);
+                redAlarmLamp.gameObject.SetActive(false);
+                StartCoroutine(FlashGreen(heartbeat));
                 break;
             case 2: // 82 bpm
                      normalScreen52.gameObject.SetActive(false); normalScreen71.gameObject.SetActive(false); normalScreen82.gameObject.SetActive(true); normalScreen107.gameObject.SetActive(false); alarmScreen121.gameObject.SetActive(false);
                 Debug.Log("screen 82 bpm");
+                StopAllCoroutines();
+                yellowAlarmLamp.gameObject.SetActive(false);
+                redAlarmLamp.gameObject.SetActive(false);
+                StartCoroutine(FlashGreen(heartbeat));
                 break;
             case 3: // 107 bpm
                      normalScreen52.gameObject.SetActive(false); normalScreen71.gameObject.SetActive(false); normalScreen82.gameObject.SetActive(false); normalScreen107.gameObject.SetActive(true); alarmScreen121.gameObject.SetActive(false);
+                StopAllCoroutines();
+                greenAlarmLamp.gameObject.SetActive(false);
+                redAlarmLamp.gameObject.SetActive(false);
+                StartCoroutine(FlashYellow());
                 Debug.Log("screen 107 bpm");
                 break;
             case 4:// 121 bpm
                     normalScreen52.gameObject.SetActive(false); normalScreen71.gameObject.SetActive(false); normalScreen82.gameObject.SetActive(false); normalScreen107.gameObject.SetActive(false); alarmScreen121.gameObject.SetActive(true);
-                StartCoroutine(Flash());
+                StopAllCoroutines();
+                greenAlarmLamp.gameObject.SetActive(false);
+                yellowAlarmLamp.gameObject.SetActive(false);
+                StartCoroutine(FlashRed());
                 Debug.Log("screen 121 bpm");
                 break;
 
@@ -69,6 +130,6 @@ public class Alarm : MonoBehaviour
 
 
 
-    }
+         }
 
 }
