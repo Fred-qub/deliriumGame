c6e27eea46055202632283429f881e9147fac4f4
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2026-04-05 02:34:19 +0100

    Subtitles on transition
    Added indication of sounds played during scene transitions, IF subtitle option is on in menu.

--- Assets/Scripts/Transistions/LevelLoader.cs
+++ Assets/Scripts/Transistions/LevelLoader.cs
@@ -11,7 +11,14 @@ public class LevelLoader : MonoBehaviour
     public AudioClip firstClip;
     public AudioClip secondClip;
 
+    [Header("Assign your subtitles here")]
+    public GameObject knock;
+    public GameObject door;
+
+
+    public int subtitles;
     private AudioSource audioSource;
+
 
     // Update is called once per frame
     void Update()
@@ -30,21 +37,46 @@ public class LevelLoader : MonoBehaviour
 
    IEnumerator PlaySoundsSequentially(int levelIndex)
     {
+        CheckSubtitleOption();
+
         audioSource = GetComponent<AudioSource>();
         transition.SetTrigger("End");
+        yield return new WaitForSeconds(1);
         // Play first clip
         audioSource.clip = firstClip;
         audioSource.Play();
-
+        if (subtitles == 1)
+        {
+            knock.SetActive(true);
+        }
+       
         // Wait until the first clip finishes
-        yield return new WaitForSeconds(firstClip.length);
+        yield return new WaitForSeconds(firstClip.length + 1);
 
         // Play second clip
         audioSource.clip = secondClip;
         audioSource.Play();
+        knock.SetActive(false);
 
-        yield return new WaitForSeconds(secondClip.length);
+        if (subtitles == 1)
+        {
+            door.SetActive(true);
+        }
+
+        yield return new WaitForSeconds(secondClip.length + 0.5f);
 
         SceneManager.LoadScene(levelIndex);
     }
+
+    void CheckSubtitleOption() 
+    {
+        if (PlayerPrefs.HasKey("Subtitles"))
+        {
+            subtitles = PlayerPrefs.GetInt("Subtitles");
+        }
+        else subtitles = 0;
+
+    }
+
+    
 }
